a4908d8f2139b80cd24bca6df7ec1be5d8f65cd2
Frias Luna <starlinluna0102@gmail.com>
2024-01-21 14:05:21 -0400

    2:05

--- practica-1/src/main/java/org/example/Main.java
+++ practica-1/src/main/java/org/example/Main.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
@@ -45,12 +46,15 @@ public class Main {
 
             NumeroDeLineas(contentType,response);
             cantParrafos(contentType,response);
+            cantImg(contentType,response);
+            cantFormularios(contentType, response);
+            inputType(contentType,response);
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 
 
 
@@ -90,6 +94,90 @@ public class Main {
             System.out.println("Número de párrafos: " + numberOfParagraphs);
         }
 
+    }
+
+
+
+    //3. Indicar la cantidad de imágenes (img) dentro de los párrafos que
+    //contiene el archivo HTML.
+    public static void cantImg(String contentType, HttpResponse response) throws IOException {
+
+        if (contentType.startsWith("text/html")) {
+
+            // Utiliza Jsoup para parsear el HTML
+            Document document = Jsoup.parse((String) response.body());
+
+            // Selecciona todos los elementos de párrafo y cuenta cuántos hay
+            Elements img = document.select("img");
+            int cantImg = img.size();
+
+            System.out.println("Número de imagenes: " + cantImg);
+        }
+
+    }
+
+
+    //4. indicar la cantidad de formularios (form) que contiene el HTML por
+    //categorizando por el método implementado POST o GET.
+
+
+    public static void cantFormularios(String contentType, HttpResponse response) throws IOException {
+
+        if (contentType.startsWith("text/html")) {
+
+            // Utiliza Jsoup para parsear el HTML
+            Document document = Jsoup.parse((String) response.body());
+
+            // Selecciona todos los elementos de párrafo y cuenta cuántos hay
+            Elements form = document.select("form");
+            int cantForm = form.size();
+
+            int postMethod = 0;
+            int getMethod = 0;
+
+            for (Element forms : form) {
+                String method = form.attr("method");
+                if (method.equalsIgnoreCase("post")) {
+                    postMethod++;
+                } else if (method.equalsIgnoreCase("get")) {
+                    getMethod++;
+                }
+            }
+
+            // Imprime el número total de formularios y cuántos usan POST y GET
+            System.out.println("Número total de formularios: " + cantForm);
+            System.out.println("Número de formularios que usan POST: " + postMethod);
+            System.out.println("Número de formularios que usan GET: " + getMethod);
+
+        }
+    }
+
+
+    //5)Para cada formulario mostrar los campos del tipo input y su
+    //respectivo tipo que contiene en el documento HTML.
+
+
+    public static void inputType(String contentType, HttpResponse response) throws IOException {
+        if (contentType.startsWith("text/html")) {
+            // Utiliza Jsoup para parsear el HTML
+            Document document = Jsoup.parse((String) response.body());
+
+            // Selecciona todos los elementos de formulario
+            Elements forms = document.select("form");
+
+            // Itera sobre los formularios
+            for (Element form : forms) {
+                // Selecciona todos los elementos de entrada dentro del formulario
+                Elements inputs = form.select("input");
+
+                // Itera sobre los elementos de entrada
+                for (Element input : inputs) {
+                    // Obtiene el atributo 'type' de cada elemento de entrada
+                    String type = input.attr("type");
+                    System.out.println("Campo de entrada encontrado con tipo: " + type);
+                }
+            }
+        }
     }
 
 }
