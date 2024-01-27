b4eb028aacdd126772d7770831a44d7c839fc407
Frias Luna <starlinluna0102@gmail.com>
2024-01-27 17:05:13 -0400

    Importando clases al main.

--- practica-2/src/main/java/org/example/Main.java
+++ practica-2/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import  org.example.clases.Articulo;
+import  org.example.clases.Comentario;
+import  org.example.clases.Usuario;
+import  org.example.clases.Etiqueta;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Etiqueta prueba = new Etiqueta(1014,"SuperHeroes");
+
+        System.out.println(prueba.getId());
+
+        prueba.setId(256);
+
+        System.out.println(prueba.getId());
+
     }
 }
