2b57cc4c4fd6c72bd3b55d1a06e15d6d53aeef7c
Frias Luna <starlinluna0102@gmail.com>
2024-02-09 17:25:43 -0400

    Falta algo.

---
+++ Asignación_de_Aula_Petición_Filtros_Recursos_Estáticos/src/main/java/org/example/Main.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Creacion de Usuario admin:
+
+
+        //Creando la instancia del servidor y configurando.
+        Javalin app = Javalin.create(config ->{
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress=false;
+                staticFileConfig.aliasCheck=null;
+
+            });
+
+        });
+        app.start(getHerokuAssignedPort());
+
+        app.get("/", cxt -> {
+            String session = cxt.sessionAttribute("currentUser");
+            if(session == null){
+                cxt.redirect("/login");
+            }else{
+                cxt.result("Buen trabajo");
+            }
+        });
+
+        app.get("/login", cxt ->{
+            cxt.redirect("/login.html");
+        });
+
+
+        //Redirrecionar a la barra.
+        app.get("/bienvenido", ctx ->{
+            if(ctx.sessionAttribute("currentUser") == null){
+                ctx.redirect("/login");
+            }else {
+                ctx.redirect("/");
+            }
+        });
+
+        app.post("/formulario", cxt ->{
+
+            String username = cxt.formParam("name");
+            String password = cxt.formParam("password");
+
+            if(username.equals("star") && password.equals("123")){
+                cxt.sessionAttribute("currentUser", username);
+                cxt.redirect("/bienvenido");
+            }else {
+                cxt.redirect("/login");
+            }
+
+        });
+
+
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 7000; //Retorna el puerto por defecto en caso de no estar en Heroku.
+    }
+
+
+
+}
