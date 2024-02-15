26109ca01db7fb70a45e3112f78909b727855e28
randaegs <garciasanchezrd@gmail.com>
2024-02-15 11:34:43 -0400

    Declarar metodo para insertar data de logueo

--- practica-3/src/main/java/org/example/jdbc/CockroachDB.java
+++ practica-3/src/main/java/org/example/jdbc/CockroachDB.java
@@ -2,13 +2,29 @@ package org.example.jdbc;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
-public class CockroachDB {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-	public static void main(String[] args) {
-		PGSimpleDataSource ds = new PGSimpleDataSource();
+public class CockroachDB {
+	private static PGSimpleDataSource ds;
+
+	private static void inicializar() {
+		ds = new PGSimpleDataSource();
 		ds.setUrl(System.getenv("JDBC_DATABASE_URL"));
 		ds.setUser("randae");
 		ds.setPassword("In8zhS6I3sRtuWAmE-R80A");
+	}
 
+	public static void insertarDataLogueo(String username) {
+		if (ds == null){
+			inicializar();
+		}
+
+		StringBuilder sb = new StringBuilder("INSERT INTO ").append("actividad")
+						.append("(usuario)")
+						.append("VALUES (?)");
+
+		String query = sb.toString();
+		PreparedStatement preparedStatement =
 	}
 }
