53c92f5fd0b8cfa5e580b24fd2d3c906aa2b06fc
randaegs <garciasanchezrd@gmail.com>
2024-02-15 16:12:20 -0400

    Arreglando variable de ambiente

--- practica-3/src/main/java/org/example/jdbc/CockroachDB.java
+++ practica-3/src/main/java/org/example/jdbc/CockroachDB.java
@@ -22,6 +22,7 @@ public class CockroachDB {
 		}
 		Connection con = ds.getConnection();
 
+		System.out.println("Este es el url: " + ds.getUrl());
 		StringBuilder sb = new StringBuilder("INSERT INTO ").append("actividad")
 						.append("(usuario)")
 						.append("VALUES (?)");
@@ -29,7 +30,9 @@ public class CockroachDB {
 		String query = sb.toString();
 		PreparedStatement preparedStatement = con.prepareStatement(query);
 		preparedStatement.setString(1, username);
+		preparedStatement.execute();
 
+		preparedStatement.close();
 		con.close();
 	}
 }
