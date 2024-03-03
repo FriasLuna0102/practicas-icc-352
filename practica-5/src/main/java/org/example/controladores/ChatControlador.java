795f7732f427a6d96bf997c22ba7a3f37a9fe2dc
randaegs <garciasanchezrd@gmail.com>
2024-03-02 22:08:28 -0400

    Anadir paginas de chat

--- practica-5/src/main/java/org/example/controladores/ChatControlador.java
+++ practica-5/src/main/java/org/example/controladores/ChatControlador.java
@@ -3,6 +3,11 @@ package org.example.controladores;
 import io.javalin.Javalin;
 import org.example.util.ControladorClass;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.javalin.apibuilder.ApiBuilder.path;
+
 public class ChatControlador extends ControladorClass {
 
 
@@ -13,5 +18,25 @@ public class ChatControlador extends ControladorClass {
 	@Override
 	public void aplicarRutas() {
 
+		app.routes(() ->{
+			path("/chat", ()-> {
+
+
+				app.post("/chat/admin", context -> {
+
+					context.render("publico/html/adminchat.html");
+				});
+
+				app.post("/chat/user", context -> {
+					String nombre = context.formParam("chateador");
+
+					Map<String,Object> modelo = new HashMap<>();
+					modelo.put("nombre", nombre);
+
+					context.render("publico/html/userchat.html", modelo);
+				});
+
+			});
+		});
 	}
 }
