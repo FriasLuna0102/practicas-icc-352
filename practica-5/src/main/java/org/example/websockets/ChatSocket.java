3093a3c3ed465908c787845ff44e55fad9a7c368
randaegs <garciasanchezrd@gmail.com>
2024-03-03 21:24:57 -0400

    Inicializar funcion para enviar mensaje a admin

--- practica-5/src/main/java/org/example/websockets/ChatSocket.java
+++ practica-5/src/main/java/org/example/websockets/ChatSocket.java
@@ -6,58 +6,72 @@ import org.example.util.ControladorClass;
 import org.thymeleaf.processor.xmldeclaration.AbstractXMLDeclarationProcessor;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ChatSocket extends ControladorClass {
 
-	private static Map<Session,String> adminSesions = new HashMap<>();
-	private static Map<Session,String> userSessions = new HashMap<>();
+    private static Map<Session,String> adminSesions = new HashMap<>();
+    private static Map<Session, Set<Session>> adminToUserSessions = new HashMap<>();
+    private static Map<Session,String> userSessions = new HashMap<>();
 
-	public ChatSocket(Javalin app) {
-		super(app);
-	}
+    public ChatSocket(Javalin app) {
+        super(app);
+    }
 
-	@Override
-	public void aplicarRutas(){
+    @Override
+    public void aplicarRutas(){
 
-		app.ws("/admin-chat", wsConfig -> {
-			wsConfig.onConnect(ctx -> {
-				String nombreAdmin = ctx.queryParam("adminName");
-				adminSesions.put(ctx.session, nombreAdmin);
-			});
+        app.ws("/admin-chat", wsConfig -> {
+            wsConfig.onConnect(ctx -> {
+                String nombreAdmin = ctx.queryParam("adminName");
+                adminSesions.put(ctx.session, nombreAdmin);
+                adminToUserSessions.put(ctx.session, new HashSet<>());
+            });
 
-			wsConfig.onMessage(ctx -> {
+            wsConfig.onMessage(ctx -> {
 
-				System.out.println("Este es el mensaje: " + ctx.message());
-			});
+                System.out.println("Este es el mensaje: " + ctx.message());
+            });
 
-			wsConfig.onClose(ctx -> {
-				adminSesions.remove(ctx.session);
-			});
-		});
+            wsConfig.onClose(ctx -> {
+                adminSesions.remove(ctx.session);
+                adminToUserSessions.remove(ctx.session);
+            });
+        });
 
-		app.ws("/user-chat", wsConfig -> {
-			wsConfig.onConnect(ctx -> {
-				String nombreUser = ctx.queryParam("username");
-				userSessions.put(ctx.session, nombreUser);
-			});
+        app.ws("/user-chat", wsConfig -> {
+            wsConfig.onConnect(ctx -> {
+                String nombreUser = ctx.queryParam("username");
+                userSessions.put(ctx.session, nombreUser);
+            });
 
-			wsConfig.onMessage(ctx -> {
-				System.out.println("Este es el mensaje: " + ctx.message());
-				enviarMensaje(ctx.message(), ctx.session);
-			});
+            wsConfig.onMessage(ctx -> {
+                System.out.println("Este es el mensaje: " + ctx.message());
 
-			wsConfig.onClose(ctx -> {
-				userSessions.remove(ctx.session);
-			});
-		});
-	}
+                enviarMensajeToAdmin(ctx.message(), ctx.session);
+            });
 
-	public static void enviarMensaje(String mensaje, Session session) throws IOException {
+            wsConfig.onClose(ctx -> {
+                userSessions.remove(ctx.session);
+            });
+        });
+    }
 
-		session.getRemote().sendString(mensaje);
-	}
+    private static void enviarMensajeToAdmin(String mensaje, Session sessionUsuario){
+
+        for (Map.Entry<Session, String> entry : adminSesions.entrySet()){
+            Session sessionAdmin = entry.getKey();
+
+            // Verificar si el administrador tiene al usuario conectado
+            Set<Session> userSessionsAdmin = adminToUserSessions.get(sessionAdmin);
+            if (userSessionsAdmin.contains(sessionUsuario)) {
+                try {
+                    // Enviar el mensaje al administrador
+                    sessionAdmin.getRemote().sendString(mensaje);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
