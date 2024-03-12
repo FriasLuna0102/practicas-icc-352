c73901a5da7eed150e5c797f0c17218a7eee97c2
Frias Luna <starlinluna0102@gmail.com>
2024-03-12 10:16:05 -0400

    Configuraciones iniciales.

--- parcial-2/src/main/java/org/example/Main.java
+++ parcial-2/src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.JavalinRenderer;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("hey");
+
+        Javalin app = Javalin.create(config ->{
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress=false;
+                staticFileConfig.aliasCheck=null;
+                JavalinRenderer.register(new JavalinThymeleaf(), ".html");
+
+            });
+
+        });
+        app.start(getHerokuAssignedPort());
+
+        app.get("/", cxt ->{
+            cxt.result("Inicio.");
+        });
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 7000; //Retorna el puerto por defecto en caso de no estar en Heroku.
+    }
+
 }
