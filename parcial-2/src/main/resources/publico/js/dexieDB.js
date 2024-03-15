295545153f2e5f392840a6b7fecf9acad509cc5b
randaegs <garciasanchezrd@gmail.com>
2024-03-15 15:56:45 -0400

    Eliminar dexie db

--- parcial-2/src/main/resources/publico/js/dexieDB.js
+++
@@ -1,8 +0,0 @@
-// Declarar base de datos
-const db = new Dexie("FormularioDB");
-db.version(1).stores({
-  registros: "++id, nombre, sector, nivelEscolar, altitud, longitud, usuario, estado",
-  usuarios: "&username, nombre, password, rol"
-});
-
-window.db = db
