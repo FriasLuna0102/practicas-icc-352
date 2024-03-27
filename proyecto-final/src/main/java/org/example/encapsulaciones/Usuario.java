8919abfe26fc3c964721cbe78d49ef646b4ffc66
randaegs <garciasanchezrd@gmail.com>
2024-03-27 18:40:52 -0400

    Inicializar encapsulaciones

---
+++ proyecto-final/src/main/java/org/example/encapsulaciones/Usuario.java
@@ -0,0 +1,13 @@
+package org.example.encapsulaciones;
+
+import java.util.List;
+
+public class Usuario {
+	private String username;
+	private String nombre;
+	private String password;
+	private boolean user;
+	private List<ShortURL> urlList;
+
+
+}
