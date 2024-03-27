df60c7b19189282a56a4941b9f0cbe8f6bc8d6b2
randaegs <garciasanchezrd@gmail.com>
2024-03-27 18:40:52 -0400

    Inicializar encapsulaciones

---
+++ proyecto-final/src/main/java/org/example/encapsulaciones/EstadisticaURL.java
@@ -0,0 +1,15 @@
+package org.example.encapsulaciones;
+
+import java.util.Date;
+import java.util.Map;
+
+public class EstadisticaURL {
+	private int cantidadAccesos;
+	private Map<String, Integer> navegadores;
+	private Map<String, Integer> direccionesIP;
+	private Map<String, Integer> dominiosClientes;
+	private Map<String, Integer> plataformasSO;
+	private Map<Date, Integer> horasAcceso;
+
+
+}
---
+++ proyecto-final/src/main/java/org/example/encapsulaciones/ShortURL.java
@@ -0,0 +1,11 @@
+package org.example.encapsulaciones;
+
+import java.util.Date;
+
+public class ShortURL {
+	private String urlBase;
+	private String urlCorta;
+	private Date fechaCreacion;
+	private EstadisticaURL estadisticas;
+	private String imgBase64;
+}
