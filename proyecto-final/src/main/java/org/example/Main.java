cf7276bbb5ae9324c49c5b4576461c0a99801623
randaegs <garciasanchezrd@gmail.com>
2024-03-27 18:26:12 -0400

    Iniciar javalin en puerto 7000

--- proyecto-final/src/main/java/org/example/Main.java
+++ proyecto-final/src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.JavalinRenderer;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import javassist.CtBehavior;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Javalin app = Javalin.create(config ->{
+			//configurando los documentos estaticos.
+			config.staticFiles.add(staticFileConfig -> {
+				staticFileConfig.hostedPath = "/";
+				staticFileConfig.directory = "/publico";
+				staticFileConfig.location = Location.CLASSPATH;
+				staticFileConfig.precompress=false;
+				staticFileConfig.aliasCheck=null;
+				JavalinRenderer.register(new JavalinThymeleaf(), ".html");
+
+			});
+
+			//Habilitando el CORS. Ver: https://javalin.io/plugins/cors#getting-started para más opciones.
+			config.plugins.enableCors(corsContainer -> {
+				corsContainer.add(corsPluginConfig -> {
+					corsPluginConfig.anyHost();
+				});
+			});
+		});
+		app.start(7000);
+
 	}
+
 }
