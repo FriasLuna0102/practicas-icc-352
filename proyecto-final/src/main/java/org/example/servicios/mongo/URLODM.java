5e09375fbf02a78f70c7bb178efe3473753c75e3
randaegs <garciasanchezrd@gmail.com>
2024-03-28 18:44:38 -0400

    Guardar url en URLODM

--- proyecto-final/src/main/java/org/example/servicios/mongo/URLODM.java
+++ proyecto-final/src/main/java/org/example/servicios/mongo/URLODM.java
@@ -1,8 +1,33 @@
 package org.example.servicios.mongo;
 
+import com.mongodb.client.MongoClients;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import org.example.encapsulaciones.ShortURL;
+import org.example.encapsulaciones.Usuario;
+
 public class URLODM {
 
+	private static URLODM instance;
+	private final Datastore datastore;
 
-	public URLODM() {
+	public URLODM getInstance(){
+		if (instance == null){
+			instance = new URLODM();
+		}
+		return instance;
+	}
+	private URLODM() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		String URL_MONGODB = processBuilder.environment().get("URL_MONGO");
+		String DB_NOMBRE = processBuilder.environment().get("DB_NOMBRE");
+
+		datastore = Morphia.createDatastore(MongoClients.create(URL_MONGODB), DB_NOMBRE);
+		datastore.getMapper().map(ShortURL.class);
+		datastore.ensureIndexes();
+	}
+
+	public void guardarURL(ShortURL shortURL){
+		datastore.save(shortURL);
 	}
 }
