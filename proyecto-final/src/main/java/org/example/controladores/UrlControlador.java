ae1d49fc9c153902478a6357fd099c0cd1a93b90
Frias Luna <starlinluna0102@gmail.com>
2024-03-28 12:56:16 -0400

    Trabajando en MongoDb.

---
+++ proyecto-final/src/main/java/org/example/controladores/UrlControlador.java
@@ -0,0 +1,22 @@
+package org.example.controladores;
+
+import io.javalin.Javalin;
+import org.example.encapsulaciones.EstadisticaURL;
+import org.example.encapsulaciones.ShortURL;
+import org.example.servicios.URLServices;
+import org.example.utils.ControladorClass;
+
+import java.util.Date;
+
+public class UrlControlador extends ControladorClass {
+    public UrlControlador(Javalin app) {
+        super(app);
+    }
+
+    @Override
+    public void aplicarRutas() {
+        EstadisticaURL esta = new EstadisticaURL();
+        Date date = new Date();
+        URLServices.getInstancia().crearUrl(new ShortURL("ddd","www.com","com",date,esta,"foto"));
+    }
+}
