c6645cd950cf43ffc115c524d0089a950ae4a41a
randaegs <garciasanchezrd@gmail.com>
2024-03-27 20:52:57 -0400

    Getters y setters

--- proyecto-final/src/main/java/org/example/encapsulaciones/ShortURL.java
+++ proyecto-final/src/main/java/org/example/encapsulaciones/ShortURL.java
@@ -1,5 +1,6 @@
 package org.example.encapsulaciones;
 
+import java.time.Instant;
 import java.util.Date;
 
 public class ShortURL {
@@ -10,4 +11,41 @@ public class ShortURL {
 	private Date fechaCreacion;
 	private EstadisticaURL estadisticas;
 	private String imgBase64;
+
+	public ShortURL(String urlBase, String imgBase64) {
+		this.urlBase = urlBase;
+		this.urlCorta = shortener(urlBase);
+		this.estadisticas = new EstadisticaURL();
+		this.imgBase64 = imgBase64;
+		this.fechaCreacion = Date.from(Instant.now());
+	}
+
+	// URL corta debe ser nombre de dominio + id
+	public String shortener(String url){
+		return "";
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getUrlBase() {
+		return urlBase;
+	}
+
+	public String getUrlCorta() {
+		return urlCorta;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public EstadisticaURL getEstadisticas() {
+		return estadisticas;
+	}
+
+	public String getImgBase64() {
+		return imgBase64;
+	}
 }
