49f403c4b4324eeddf5a75452d647b287ceebb54
randaegs <garciasanchezrd@gmail.com>
2024-04-01 00:22:33 -0400

    Autologin con jwt

--- proyecto-final/src/main/java/org/example/controladores/IndexControlador.java
+++ proyecto-final/src/main/java/org/example/controladores/IndexControlador.java
@@ -1,9 +1,13 @@
 package org.example.controladores;
 
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import io.javalin.Javalin;
 import org.example.encapsulaciones.Usuario;
 import org.example.servicios.UsuarioServices;
+import org.example.servicios.mongo.UsuarioODM;
 import org.example.utils.ControladorClass;
+import org.example.utils.JWTutils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,8 +23,13 @@ public class IndexControlador extends ControladorClass {
 
 		app.get("/", context -> {
 
-			Usuario usuario = UsuarioServices.getInstancia().getUsuarioLogueado();
+			String jwt = context.cookie("jwt");
+			if (jwt != null && UsuarioServices.getInstancia().getUsuarioLogueado() == null){
+				Claim username = JWTutils.decodeJWT(jwt).getClaim("username");
+				UsuarioServices.getInstancia().setUsuarioLogueado(UsuarioODM.getInstance().buscarUsuarioByUsername(username.asString()));
+			}
 
+			Usuario usuario = UsuarioServices.getInstancia().getUsuarioLogueado();
 			Map<String,Object> model = new HashMap<>();
 			model.put("usuario", usuario);
 
--- proyecto-final/src/main/java/org/example/utils/JWTutils.java
+++ proyecto-final/src/main/java/org/example/utils/JWTutils.java
@@ -10,10 +10,9 @@ import java.util.Date;
 public class JWTutils {
 
 	private static final String SECRET_KEY = "ccoK4iHM^qBHpwwvG^";
-	private static final long EXPIRATION_TIME = 86400000; // Tiempo de expiración del token (en milisegundos, aquí es un día)
 
 	public static String generateJwt(String username) throws UnsupportedEncodingException {
-		Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY); // Reemplaza "your-secret" con tu secreto
+		Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 		String token = JWT.create()
 				.withIssuer("icc352")
 				.withSubject("proyecto")
@@ -23,9 +22,9 @@ public class JWTutils {
 	}
 
 	public static DecodedJWT decodeJWT(String jwt) throws UnsupportedEncodingException {
-		Algorithm algorithm = Algorithm.HMAC256("your-secret"); // Reemplaza "your-secret" con tu secreto
+		Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 		DecodedJWT decodedJWT = JWT.require(algorithm)
-				.withIssuer("auth0")
+				.withIssuer("icc352")
 				.build()
 				.verify(jwt);
 		return decodedJWT;
