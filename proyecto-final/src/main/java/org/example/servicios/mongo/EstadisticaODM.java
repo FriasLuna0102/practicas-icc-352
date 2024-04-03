6cfeee154994971122bfdcd27d3f3e91e5eb4554
randaegs <garciasanchezrd@gmail.com>
2024-04-03 19:17:56 -0400

    Crear clase ODM de estadistica

---
+++ proyecto-final/src/main/java/org/example/servicios/mongo/EstadisticaODM.java
@@ -0,0 +1,33 @@
+package org.example.servicios.mongo;
+
+import com.mongodb.client.MongoClients;
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+import org.example.encapsulaciones.EstadisticaURL;
+import org.example.encapsulaciones.ShortURL;
+
+public class EstadisticaODM {
+	private static EstadisticaODM instance;
+	private final Datastore datastore;
+
+	public static EstadisticaODM getInstance(){
+		if (instance == null){
+			instance = new EstadisticaODM();
+		}
+		return instance;
+	}
+	private EstadisticaODM() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		String URL_MONGODB = processBuilder.environment().get("URL_MONGO");
+		String DB_NOMBRE = processBuilder.environment().get("DB_NOMBRE");
+
+		datastore = Morphia.createDatastore(MongoClients.create(URL_MONGODB), DB_NOMBRE);
+		datastore.getMapper().map(EstadisticaURL.class);
+		datastore.ensureIndexes();
+	}
+
+	public void guardarEstadistica(EstadisticaURL estadisticaURL){
+		datastore.save(estadisticaURL);
+	}
+
+}
