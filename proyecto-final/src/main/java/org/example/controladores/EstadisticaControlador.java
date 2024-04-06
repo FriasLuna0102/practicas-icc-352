90dc827faa6e367d7541a463107749eafd1ef30d
randaegs <garciasanchezrd@gmail.com>
2024-04-05 20:42:53 -0400

    Crear clase de controlador para estadistica

---
+++ proyecto-final/src/main/java/org/example/controladores/EstadisticaControlador.java
@@ -0,0 +1,16 @@
+package org.example.controladores;
+
+import io.javalin.Javalin;
+import org.example.utils.ControladorClass;
+
+public class EstadisticaControlador extends ControladorClass {
+
+	public EstadisticaControlador(Javalin app){
+		super(app);
+	}
+
+	@Override
+	public void aplicarRutas() {
+
+	}
+}
