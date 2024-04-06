e4f3a8af20d19abbea6181e83e627ec4b5527a52
randaegs <garciasanchezrd@gmail.com>
2024-04-06 00:08:24 -0400

    Crear graficos base

---
+++ proyecto-final/src/main/resources/publico/js/graficoDias.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('graficoDias');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'],
+        datasets: [{
+            label: 'Cantidad total de accesos por dia',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
+});
---
+++ proyecto-final/src/main/resources/publico/js/graficoHoras.js
@@ -0,0 +1,21 @@
+const ctx2 = document.getElementById('graficoHoras');
+
+new Chart(ctx2, {
+    type: 'bar',
+    data: {
+        labels: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'],
+        
+        datasets: [{
+            label: 'Cantidad total de accesos por hora',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
+});
