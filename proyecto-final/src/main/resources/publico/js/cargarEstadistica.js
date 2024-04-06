ec2fea208b042714d77c7fa0a9d8843b0078e175
randaegs <garciasanchezrd@gmail.com>
2024-04-05 20:43:09 -0400

    Modificar script de cargar estadistica

--- proyecto-final/src/main/resources/publico/js/cargarEstadistica.js
+++ proyecto-final/src/main/resources/publico/js/cargarEstadistica.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".cargarContenido").click(function(e){
+    $(".link").click(function(e){
         e.preventDefault(); // Previene la acción predeterminada del enlace
         var pagina = $(this).attr('href'); // Obtiene la URL de la página vinculada
         $("#contenido").load(pagina); // Carga el contenido de la página en el div con el id "contenido"
