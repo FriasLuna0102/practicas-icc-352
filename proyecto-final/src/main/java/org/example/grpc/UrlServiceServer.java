8102aeac537e1cb18b8e933a5be5b797a27b2ba4
randaegs <garciasanchezrd@gmail.com>
2024-04-12 11:53:22 -0400

    Subir proto

---
+++ proyecto-final/src/main/java/org/example/grpc/UrlServiceServer.java
@@ -0,0 +1,20 @@
+package org.example.grpc;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class UrlServiceServer {
+
+	/*
+	public static void main(String[] args) throws Exception {
+		Server server = ServerBuilder.forPort(8000)
+				.addService()
+				.build();
+
+		server.start();
+		System.out.println("Servidor grpc iniciado");
+		server.awaitTermination();
+	}
+
+	 */
+
+}
